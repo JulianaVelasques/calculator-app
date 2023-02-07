4b1703d90b02003b7b4244adecb86ccebfd99775
JulianaVelasques <juliabalta16@gmail.com>
2023-02-06 22:19:50 -0300

    feat: add icons and create a file to import all of them

---
+++ src/export/icon.ts
@@ -0,0 +1,19 @@
+import divider from '../assets/divider.png';
+import equals from '../assets/equals.png';
+import percent from '../assets/percent.png';
+import plusMinus from '../assets/plusMinus.png';
+import times from '../assets/times.png';
+import minus from '../assets/minus.png';
+import plus from '../assets/plus.png';
+import bigEquals from '../assets/bigEquals.png';
+
+export default {
+  divider: divider,
+  equals: equals,
+  percent: percent,
+  plusMinus: plusMinus,
+  times: times,
+  minus: minus,
+  plus: plus,
+  bigEquals: bigEquals,
+};
